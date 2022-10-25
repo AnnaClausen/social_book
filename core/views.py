c63709fbd5df2733e4f3d0cb8c7b0d5d4f418630
Anna Clausen <anna.revall.clausen@gmail.com>
2022-10-25 13:52:05 -0700

    configure login and logout functionality

--- core/views.py
+++ core/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'settings.html')
 
 def signup(request):
     if request.method == 'POST': 
@@ -27,7 +34,11 @@ def signup(request):
                 user.save()
 
                 #Log user in and redirect to settings page
-                #create a profile object for the new user
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+
+                #Create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
@@ -38,3 +49,27 @@ def signup(request):
 
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('signin')
+
+
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')       
